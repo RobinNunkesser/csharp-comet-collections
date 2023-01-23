7f43e18b8ad0df5c382183ee2407c7cf184a7a46
Robin Nunkesser <robin.nunkesser@hshl.de>
2023-01-23 10:49:09 +0100

    Initial commit

---
+++ ListsComet/SectionViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListsComet
+{
+    public class SectionViewModel<T> : List<T>
+    {
+        public string Header { get; set; } = string.Empty;
+    }
+}
